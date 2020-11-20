b6a67ab743f3765c22f7c4edee47ddc4b5378322
SistemaJHCP <it@jhcp.com>
2020-11-20 13:12:20 -0400

    prisa

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -12,14 +12,53 @@
 @endsection
 
 @section('body')
+<div class="col-md-4">
+    <div style="background: white; padding:15px;">
+        <p class="text-center">
+            <strong>Goal Completion</strong>
+        </p>
 
+        <div class="progress-group">
+            <span class="progress-text">Add Products to Cart</span>
+            <span class="progress-number"><b>160</b>/200</span>
 
+            <div class="progress sm">
+            <div class="progress-bar progress-bar-aqua" style="width: 90%"></div>
+            </div>
+        </div>
+        <!-- /.progress-group -->
+        <div class="progress-group">
+            <span class="progress-text">Complete Purchase</span>
+            <span class="progress-number"><b>310</b>/400</span>
 
+            <div class="progress sm">
+            <div class="progress-bar progress-bar-red" style="width: 80%"></div>
+            </div>
+        </div>
+        <!-- /.progress-group -->
+        <div class="progress-group">
+            <span class="progress-text">Visit Premium Page</span>
+            <span class="progress-number"><b>480</b>/800</span>
 
+            <div class="progress sm">
+            <div class="progress-bar progress-bar-green" style="width: 80%"></div>
+            </div>
+        </div>
+        <!-- /.progress-group -->
+        <div class="progress-group">
+            <span class="progress-text">Send Inquiries</span>
+            <span class="progress-number"><b>250</b>/500</span>
 
-
-
-
+            <div class="progress sm">
+            <div class="progress-bar progress-bar-yellow" style="width: 80%"></div>
+            </div>
+        </div>
+        <!-- /.progress-group -->
+    </div>
+</div>
+<div class="col-md-8">
+    trampa
+</div>
 @endsection
 @section('js')
 
